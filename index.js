2857890d1aa474adb1c80df6d6b7cadcec2d5d2a
cao161 <120444178+cao161@users.noreply.github.com>
2023-07-07 17:27:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mainContentLetter = "Gửi lời nhắn nhủ đến người bạn bạn 
 
 // Gắn 1 đường link ảnh bất kì
 let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư
-imgStart.src = "./img/cute-young-boy-kid-wearing-vest-and-hat-free-png.png";
+imgStart.src = "./img/b4bbdb54b7152338d7143cb444a77f09.png";
 
 // Gắn 1 link ảnh bất kì
 let imgLetter = document.querySelector(".img");
